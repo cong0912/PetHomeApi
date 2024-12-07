e2e770093d7c46a64ad868cd0ad15c995801ccfc
cong0912 <congntc912@gmail.com>
2024-12-07 12:50:05 +0700

    add

--- src/server.js
+++ src/server.js
@@ -1,26 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-require('dotenv').config();
-import connection from './config/connectDB';
-import apiRoutes from './api/routes';
+import express from "express";
+import bodyParser from "body-parser";
+require("dotenv").config();
+import connection from "./config/connectDB";
+import apiRoutes from "./api/routes";
 
 const app = express();
 
 app.use(function (req, res, next) {
-    let method = req.method;
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  let method = req.method;
+  const allowedOrigins = process.env.ALLOWED_ORIGINS || "http://localhost:3000";
+  const origin = req.headers.origin;
 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  if (allowedOrigins.split(",").includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Authorization");
 
-    if (method == 'OPTIONS') {
-        return res.sendStatus(200);
-    }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    next();
+  if (method == "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
 });
 
 app.use(bodyParser.json());
